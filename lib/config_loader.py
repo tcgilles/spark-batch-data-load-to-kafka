4aa69146f7aa8ce8998bd8dba1b11d410ca47f57
Gilles Takam <chenogilles@gmail.com>
2025-09-23 23:46:24 +0200

    Added type hints in config_loader

--- lib/config_loader.py
+++ lib/config_loader.py
@@ -1,20 +1,22 @@
+from typing import Any
 import configparser
+
+import pyspark
 from pyspark import SparkConf
 
-def get_config(env):
-    config = configparser.ConfigParser()
+def get_config(env: str) -> dict[str, Any]:
+    config: configparser.ConfigParser = configparser.ConfigParser()
     config.read("conf/sbdl.conf")
-    conf = {}
+    conf: dict[str, Any]  = {}
     for (key, val) in config.items(env):
         conf[key] = val
     return conf
 
 
-def get_spark_conf(env):
-    spark_conf = SparkConf()
-    config = configparser.ConfigParser()
+def get_spark_conf(env: str) -> pyspark.SparkConf:
+    spark_conf: pyspark.SparkConf = SparkConf()
+    config: configparser.ConfigParser = configparser.ConfigParser()
     config.read("conf/spark.conf")
-
     for (key, val) in config.items(env):
         spark_conf.set(key, val)
     return spark_conf
