ef14ec36ba3f6093bcc4324325855f7dc3fae9dd
Gilles Takam <chenogilles@gmail.com>
2025-09-28 15:41:45 +0200

    Modified spark.conf

--- sbdl_submit.sh
+++ sbdl_submit.sh
@@ -1,4 +1,7 @@
 spark-submit --master yarn --deploy-mode cluster \
 --py-files sdbl_lib.zip \
 --files conf/sdbl.conf,conf/spark.conf,log4j.properties \
+--driver-cores 2 \
+--driver-memory 3G \
+--conf spark.driver.memoryOverhead=1G
 main.py qa 2025-09-23
